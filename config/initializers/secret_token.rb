75590dbfea72a652c97b6d694dcac18efe750072
William Graham <liamwgraham@gmail.com>
2013-10-12 22:58:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liamgraham::Application.config.secret_key_base = '3acbac48fd7e2352f6adf50948164c95ede3fd98caa94007a602dfa1faa38f13ebb5af79474c14b5a82239dac7a2941ec11554b91015c8a935f8a887acc3d4fe'

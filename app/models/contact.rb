7e15dd98289e5159d2e3a03ca0176cc4d36ea880
William Graham <liamwgraham@gmail.com>
2013-10-14 21:52:20 -0500

    save data to a spreadsheet

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,4 +11,20 @@ class Contact < ActiveRecord::Base
   validates_format_of :email,
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
+
+  def update_spreadsheet
+    connection = GoogleDrive.login(ENV["GMAIL_USERNAME"], ENV["GMAIL_PASSWORD"])
+    ss = connection.spreadsheet_by_title('Liam_Graham_Contacts')
+    if ss.nil?
+      ss = connection.create_spreadsheet('Liam_Graham_Contacts')
+    end
+
+    ws = ss.worksheets[0]
+    last_row = ws.num_rows + 1
+    ws[last_row, 1] = Time.new
+    ws[last_row, 2] = self.name
+    ws[last_row, 2] = self.email
+    ws[last_row, 3] = self.content
+    ws.save
+  end
 end
